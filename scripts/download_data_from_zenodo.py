fc6f23f08503d26f6e0073656fdfdba0ca647f26
jules-vanaret <jules.vanaret@univ-amu.fr>
2025-01-28 16:34:59 +0100

    updated with zenodo link

--- scripts/download_data_from_zenodo.py
+++ scripts/download_data_from_zenodo.py
@@ -2,16 +2,17 @@ import requests
 import zipfile
 from tqdm import tqdm
 import pathlib
-import os
-    
 
 
-if __name__ == '__main__':
-    script_dir = pathlib.Path(__file__).parents[1] / 'data'
+if __name__ == "__main__":
+    script_dir = pathlib.Path(__file__).parents[1] / "data"
     print(script_dir)
 
     # url of the data
-    url=requests.get('...', stream=True)
+    url = requests.get(
+        "https://zenodo.org/records/14748083/files/zenodo_tapenade_data.zip?download=1",
+        stream=True,
+    )
 
     # Sizes in bytes
     block_size = 1024
@@ -19,16 +20,25 @@ if __name__ == '__main__':
     # Create the zip file
     filename = "file.zip"
 
-    with tqdm(total=0, unit='B', unit_scale=True) as pbar:
+    with tqdm(
+        unit="B",
+        unit_scale=True,
+        total=int(9.41e9),
+        desc="Downloading data from Zenodo:",
+    ) as pbar:
         with open(script_dir / filename, mode="wb") as localfile:
             for data in url.iter_content(block_size):
                 pbar.update(len(data))
                 localfile.write(data)
 
-    with zipfile.ZipFile(script_dir / filename, 'r') as zip_ref:
-        zip_ref.extractall(os.path.join(script_dir, './'))
+    with zipfile.ZipFile(script_dir / filename, "r") as zip_ref:
+        for file in tqdm(
+            zip_ref.namelist(),
+            total=len(zip_ref.namelist()),
+            desc="Extracting data from zip file:",
+        ):
+            zip_ref.extract(member=file, path=script_dir)
 
     # remove initial folder
     folder_path = script_dir / filename
     folder_path.unlink()
-    
